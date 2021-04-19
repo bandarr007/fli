5738ea5578098357314b9c9f608a06c1453a9236
bandarr007 <82390155+bandarr007@users.noreply.github.com>
2021-04-19 22:13:12 +0300

    Add files via upload

--- Files_ban/TagAdmin.lua
+++ Files_ban/TagAdmin.lua
@@ -1,68 +1,42 @@
 local function TagAd(msg)
 local text = msg.content_.text_
-if text == 'تفعيل @all' and Owner(msg) then   
-redis:del(bot_id..'Tag:Admins'..msg.chat_id_) 
-Text = '\n اهلا عزيزي \n تم تفعيل امر @all' 
-send(msg.chat_id_, msg.id_,Text) 
+if text and text == "تفعيل تاك الادمنيه" and Owner(msg) then 
+database:set(bot_id.."Tag:Admins:"..msg.chat_id_,true)
+send(msg.chat_id_, msg.id_,"⌔︙تم تفعيل تاك الادمنيه")
 end
-if text == 'تعطيل @all' and Owner(msg) then  
-redis:set(bot_id..'Tag:Admins'..msg.chat_id_,true) 
-Text = '\nاهلا عزيزي \n تم تعطيل امر @all' 
-send(msg.chat_id_, msg.id_,Text) 
-end 
-if text == ("@all") and Owner(msg) and not redis:get(bot_id..'Tag:Admins'..msg.chat_id_) then
-tdcli_function({ID="GetChannelFull",channel_id_ = msg.chat_id_:gsub('-100','')},function(argg,dataa) 
-tdcli_function({ID = "GetChannelMembers",channel_id_ = msg.chat_id_:gsub('-100',''), offset_ = 0,limit_ = dataa.member_count_
-},function(ta,NightRang)
-x = 0
-tags = 0
-local list = NightRang.members_
-for k, v in pairs(list) do
-tdcli_function({ID="GetUser",user_id_ = v.user_id_},function(arg,data)
-if x == 5 or x == tags or k == 0 then
-tags = x + 5
-t = ""
+if text and text == "تعطيل تاك الادمنيه" and Owner(msg) then 
+database:del(bot_id.."Tag:Admins:"..msg.chat_id_)
+send(msg.chat_id_, msg.id_, "⌔︙تم تعطيل تاك الادمنيه")
 end
-x = x + 1
-tagname = data.first_name_
-tagname = tagname:gsub("[[]","")
-t = t..", ["..tagname.."](tg://user?id="..v.user_id_..")"
-if x == 5 or x == tags or k == 0 then
-local Text = t:gsub(' ,','')
-sendText(msg.chat_id_,Text,msg.id_/2097152/0.5,'md')
+
+if text == 'صيح الادمنيه' or text == "تاك للادمنيه" or text == "وين الادمنيه" then
+if database:get(bot_id.."Tag:Admins:"..msg.chat_id_) then 
+tdcli_function ({ID = "GetChannelMembers",channel_id_ = msg.chat_id_:gsub("-100",""),filter_ = {ID = "ChannelMembersAdministrators"},offset_ = 0,limit_ = 100},function(a,data)
+tdcli_function ({ID = "GetUser",user_id_ = msg.sender_user_id_},function(arg,b)  
+if b.username_ then 
+User_id = "@"..b.username_
+else
+User_id = msg.sender_user_id_
 end
-end,nil)
+local t = "\n⌔︙المستخدم ~ ["..User_id .."] يصيح الادمنيه \n\n"
+k = 0
+for i,v in pairs(data.members_) do
+if bot_id ~= v.user_id_ then 
+k = k + 1
+local username = database:get(bot_id.."User:Name"..v.user_id_)
+if username then
+t = t..""..k.."- ([@"..username.."])\n"
+else
+t = t..""..k.."- (`"..v.user_id_.."`)\n"
 end
-end,nil)
-end,nil)
 end
-if text and text:match('@all (.*)') and Owner(msg) and not redis:get(bot_id..'Tag:Admins'..msg.chat_id_) then
-tdcli_function({ID="GetChannelFull",channel_id_ = msg.chat_id_:gsub('-100','')},function(argg,dataa) 
-tdcli_function({ID = "GetChannelMembers",channel_id_ = msg.chat_id_:gsub('-100',''), offset_ = 0,limit_ = dataa.member_count_
-},function(ta,NightRang)
-x = 0
-tags = 0
-local list = NightRang.members_
-for k, v in pairs(list) do
-tdcli_function({ID="GetUser",user_id_ = v.user_id_},function(arg,data)
-if x == 5 or x == tags or k == 0 then
-tags = x + 5
-t = ""
 end
-x = x + 1
-tagname = data.first_name_
-tagname = tagname:gsub("]","")
-tagname = tagname:gsub("[[]","")
-t = t..", ["..tagname.."](tg://user?id="..v.user_id_..")"
-if x == 5 or x == tags or k == 0 then
-local msg_id = msg.id_/2097152/0.5
-https.request("https://api.telegram.org/bot"..token..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(text:match('@all (.*)')..'\n'..t).."&parse_mode=Markdown&reply_to_message_id="..msg_id)
-end
-end,nil)
-end
+send(msg.chat_id_, msg.id_,t)
 end,nil)
 end,nil)
 end
+end   
+
 
 end
 return {B = TagAd}
